a9c6e69df82cfc9f355e828484683a511374d62f
Dmytro POLOSUKHIN <dpolosuk@e1r3p7.unit.ua>
2018-04-30 17:06:34 +0300

    added draggable functionality to template images

--- index.php
+++ index.php
@@ -72,114 +72,115 @@
 					<img id="dog-looking-right" class="img-carousel" src="img/5845e770fb0b0755fa99d7f4.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/5845ec53dda95a5696fa1a26.png">
+					<img id="purple-frog-looking-left" class="img-carousel" src="img/5845ec53dda95a5696fa1a26.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/5845ec62dda95a5696fa1a27.png">
+					<img id="purple-frog-looking-right" class="img-carousel" src="img/5845ec62dda95a5696fa1a27.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/5845ec7fdda95a5696fa1a28.png">
+					<img id="green-frog-with-tiny-legs" class="img-carousel" src="img/5845ec7fdda95a5696fa1a28.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/5845ec9fdda95a5696fa1a29.png">
+					<img id="green-frog-with-chest" class="img-carousel" src="img/5845ec9fdda95a5696fa1a29.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/5845ed1bdda95a5696fa1a2a.png">
+					<img id="green-frog-head-only" class="img-carousel" src="img/5845ed1bdda95a5696fa1a2a.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/584997587422de75c36182d4.png">
+					<img id="chain" class="img-carousel" src="img/584997587422de75c36182d4.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/5849978e7422de75c36182d8.png">
+					<img id="chain-with-dollar" class="img-carousel" src="img/5849978e7422de75c36182d8.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/584998d695008575ff974862.png">
+					<img id="weed-cigaret" class="img-carousel" src="img/584998d695008575ff974862.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/58499929f6ff2e760b7467a8.png">
+					<img id="smoking-cigaret" class="img-carousel" src="img/58499929f6ff2e760b7467a8.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/584999937b7d4d76317f5ffd.png">
+					<img id="pixel-glasses" class="img-carousel" src="img/584999937b7d4d76317f5ffd.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/584999a27b7d4d76317f5ffe.png">
+					<img id="pixel-glasses-looging-left" class="img-carousel" src="img/584999a27b7d4d76317f5ffe.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/58499c84e9678476f613457a.png">
+					<img id="thug-life-with-two-guns-tatoo" class="img-carousel" src="img/58499c84e9678476f613457a.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/58499e29b89d73775876620d.png">
+					<img id="thug-life-camel" class="img-carousel" src="img/58499e29b89d73775876620d.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/58499f7926191c779d22d92f.png">
+					<img id="obey-hat" class="img-carousel" src="img/58499f7926191c779d22d92f.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/58499f8b26191c779d22d931.png">
+					<img id="thug-life-red-hat" class="img-carousel" src="img/58499f8b26191c779d22d931.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/58499f9726191c779d22d932.png">
+					<img id="thug-life-bandana-black" class="img-carousel" src="img/58499f9726191c779d22d932.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/58499fc926191c779d22d933.png">
+					<img id="thug-life-bandana-red" class="img-carousel" src="img/58499fc926191c779d22d933.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/5849a03726191c779d22d936.png">
+					<img id="thug-life-hat-black" class="img-carousel" src="img/5849a03726191c779d22d936.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/585059646a5ae41a83ddee9c.png">
+					<img id="trump-hair-front" class="img-carousel" src="img/585059646a5ae41a83ddee9c.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/585059b56a5ae41a83ddee9d.png">
+					<img id="trumg-hair-profile" class="img-carousel" src="img/585059b56a5ae41a83ddee9d.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/5859b173711f64423aa5e050.png">
+					<img id="pepe-smiling-humble" class="img-carousel" src="img/5859b173711f64423aa5e050.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/5862d43c1aa68e0bf26fa200.png">
+					<img id="obama-on-the-phone" class="img-carousel" src="img/5862d43c1aa68e0bf26fa200.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/586567ea7d90850fc3ce2a29.png">
+					<img id="robot-head" class="img-carousel" src="img/586567ea7d90850fc3ce2a29.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/588a6acad06f6719692a2d28.png">
+					<img id="computer-low" class="img-carousel" src="img/588a6acad06f6719692a2d28.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/588a6ad0d06f6719692a2d29.png">
+					<img id="computer-right" class="img-carousel" src="img/588a6ad0d06f6719692a2d29.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/588a6b2ad06f6719692a2d2a.png">
+					<img id="laptop" class="img-carousel" src="img/588a6b2ad06f6719692a2d2a.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/58909b545236a4e0f6e2f975.png">
+					<img id="just-do-it-front" class="img-carousel" src="img/58909b545236a4e0f6e2f975.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/58909bfe5236a4e0f6e2f97a.png">
+					<img id="just-do-it-profile" class="img-carousel" src="img/58909bfe5236a4e0f6e2f97a.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/58967e8c0803320bf17c2fb7.png">
+					<img id="nigga-crying" class="img-carousel" src="img/58967e8c0803320bf17c2fb7.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/5896f72ecba9841eabab60fe.png">
+					<img id="pepe-white" class="img-carousel" src="img/5896f72ecba9841eabab60fe.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/589b4da582250818d81e748f.png">
+					<img id="think-of-it-nigga" class="img-carousel" src="img/589b4da582250818d81e748f.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/59401c27bd0e8b4eeeb603c3.png">
+					<img id="imac" class="img-carousel" src="img/59401c27bd0e8b4eeeb603c3.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/5984a0f14ca2e7219dcbe493.png">
+					<img id="napoleon-hat" class="img-carousel" src="img/5984a0f14ca2e7219dcbe493.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/59cfce69d3b1936210a5ddf6.png">
+					<img id="little-plant" class="img-carousel" src="img/59cfce69d3b1936210a5ddf6.png">
 				</div>
 				<div class="img-wrapper">
-					<img class="img-carousel" src="img/59cfce7ad3b1936210a5ddf7.png">
+					<img id="bay-leaf" class="img-carousel" src="img/59cfce7ad3b1936210a5ddf7.png">
 				</div>
 			</div>
 		</div>
 
 		<?php require_once "footer.php"; ?>
 		<script src="js/camera_setup.js"> </script>
+		<script src="js/draggable.js"> </script>
 	</body>
 </html>
---
+++ js/draggable.js
@@ -0,0 +1,58 @@
+var startX = 0;
+var startY = 0;
+var offsetX = 0;
+var offsetY = 0;
+var dragElement = 0;
+
+
+
+initDragDrop();
+
+function initDragDrop() {
+	document.onmousedown = onMouseDown;
+	document.onmouseup = onMouseUp;
+}
+
+function onMouseDown(e) {
+	var target = e.target;
+
+	if ((e.button == 1 && window.event != null ||
+		e.button == 0) && target.className == 'template-img') {
+		startX = e.clientX;
+		startY = e.clientY;
+		offsetX = extractNumber(target.style.left);
+		offsetY = extractNumber(target.style.top);
+		oldZIndex = target.style.zIndex;
+		dragElement = target;
+		document.onmousemove = onMouseMove;
+		document.body.focus();
+		document.onselectstart = function () { return false; };
+		target.ondragstart = function() { return false; };
+		return false;
+	}
+}
+
+function extractNumber(value) {
+	var n = parseInt(value);
+
+	return n == null || isNaN(n) ? 0 : n;
+}
+
+function onMouseMove(e) {
+	if (e == null) {
+		var e = window.event;
+	}
+	dragElement.style.left = (offsetX + e.clientX - startX) + 'px';
+	console.log('e.clientX: ' + e.clientX);
+	dragElement.style.top = (offsetY + e.clientY - startY) + 'px';
+	console.log('e.clientY: ' + e.clientY);
+}
+
+function onMouseUp(e) {
+	if (dragElement != null) {
+		document.onmousemove = null;
+		document.onselectstart = null;
+		dragElement.ondragstart = null;
+		dragElement = null;
+	}
+}
